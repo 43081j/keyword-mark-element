55f672eb2716d8ac93202583b8a5ae5e2127cd66
43081j <43081j@users.noreply.github.com>
2019-08-05 21:20:30 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# `<keyword-mark>`
+
+A simple web component to mark keywords in a string.
+
+## Usage
+
+Install:
+
+```
+$ npm i -S keyword-mark-element
+```
+
+Include:
+
+```html
+<script src="node_modules/keyword-mark-element/lib/keyword-mark.js"></script>
+```
+
+Or in JavaScript:
+
+```ts
+import "./node_modules/keyword-mark-element/lib/keyword-mark.js";
+```
+
+Use:
+
+```html
+<keyword-mark keywords="foo">foo bar baz</keyword-mark>
+```
+
+Essentially, any keywords specified in the `keywords` attribute will be
+highlighted in the text content of the element.
+
+## License
+
+MIT
---
+++ src/main.ts
@@ -0,0 +1,134 @@
+const escapeRegexChars = [
+  '-',
+  '[',
+  ']',
+  '/',
+  '{',
+  '}',
+  '(',
+  ')',
+  '*',
+  '+',
+  '?',
+  '.',
+  '\\',
+  '^',
+  '$',
+  '|',
+];
+const escapeRegexPattern = new RegExp(`[${escapeRegexChars.join('\\')}]`, 'g');
+const escapeRegex = (str: string): string =>
+  str.replace(escapeRegexPattern, '\\$&');
+
+/**
+ * Renders a string with keywords highlighted
+ */
+export class KeywordMarkElement extends HTMLElement {
+  /** @inheritdoc */
+  public static get observedAttributes(): string[] {
+    return ['keywords'];
+  }
+
+  /**
+   * Keywords to highlight
+   */
+  public get keywords(): string {
+    return this.getAttribute('keywords') || '';
+  }
+
+  /**
+   * Sets the keywords to highlight
+   * @param {string} val keywords to set
+   */
+  public set keywords(val: string) {
+    this.setAttribute('keywords', val);
+  }
+
+  /**
+   * Observer of child nodes
+   */
+  private __observer?: MutationObserver;
+
+  /** constructor */
+  public constructor() {
+    super();
+
+    this.attachShadow({mode: 'open'});
+  }
+
+  /** @inheritdoc */
+  public attributeChangedCallback(
+    name: string,
+    oldValue: string,
+    newValue: string
+  ): void {
+    if (name === 'keywords') {
+      if (newValue !== oldValue) {
+        this._render();
+      }
+    }
+  }
+
+  /** @inheritdoc */
+  public connectedCallback(): void {
+    this.__observer = new MutationObserver(() => {
+      this._render();
+    });
+
+    this.__observer.observe(this, {childList: true});
+  }
+
+  /** @inheritdoc */
+  public disconnectedCallback(): void {
+    if (this.__observer) {
+      this.__observer.disconnect();
+      this.__observer = undefined;
+    }
+  }
+
+  /**
+   * Renders the current string with keywords highlighted
+   */
+  protected _render(): void {
+    if (!this.shadowRoot) {
+      return;
+    }
+
+    const text = this.textContent || '';
+    const keywords = this.getAttribute('keywords');
+
+    if (!keywords) {
+      this.shadowRoot.textContent = text;
+      return;
+    }
+
+    const lowerText = text.toLowerCase();
+    const terms = keywords.toLowerCase().split(/\s+/);
+    const splitPattern =
+      new RegExp(`${terms.map(escapeRegex).join('|')}`, 'gi');
+    const parts = text.split(splitPattern);
+    const result = document.createElement('div');
+
+    let cursor = 0;
+
+    for (const part of parts) {
+      result.appendChild(document.createTextNode(part));
+      cursor += part.length;
+
+      if (cursor < lowerText.length) {
+        const slice = lowerText.substring(cursor);
+        const match = terms.find((t) => slice.startsWith(t));
+        if (match) {
+          const mark = document.createElement('mark');
+          mark.textContent = text.substr(cursor, match.length);
+          result.appendChild(mark);
+          cursor += match.length;
+        }
+      }
+    }
+
+    this.shadowRoot.innerHTML = result.innerHTML;
+  }
+}
+
+customElements.define('keyword-mark', KeywordMarkElement);
